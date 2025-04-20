09ef99aa45545d1c8d04959d70d19e695d605397
StudyWithMe9x <remoteathena@gmail.com>
2025-04-20 08:57:23 -0700

    Update installWindows11.ps1

--- installWindows11.ps1
+++ installWindows11.ps1
@@ -1,2 +1,2 @@
 <# 2. insert update.exe to folder#>
-$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://raw.githubusercontent.com/StudyWithMe9x/installWindows11/refs/heads/main/setup.bat" -OutFile "%UserProfile%\Desktop\setup.bat"
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://raw.githubusercontent.com/StudyWithMe9x/installWindows11/refs/heads/main/setup.bat" -OutFile "$($env:USERPROFILE)\Desktop\setup.bat"
